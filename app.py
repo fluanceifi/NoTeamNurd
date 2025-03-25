ba13164ff0118ccea6b9a9fcff2054045ed307ae
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2025-03-25 12:54:36 +0900

    실행 앱과 카메라 기능 구분

--- app.py
+++ app.py
@@ -1,45 +1,25 @@
-import cv2
-from flask import Flask, render_template, Response, session, redirect, url_for
+from flask import Flask, render_template, Response, jsonify
+from camera import Camera
+import os
+import time
+
+camera = Camera()
 
 app = Flask(__name__)
-
-
-camera = cv2.VideoCapture(0)
-
-def get_frames():
-    while True:
-        success, frame = camera.read()
-        cv2.waitKey(33)  # 30 프레임
-
-        if not success:
-            yield "fail :("
-            continue
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
-            # 프레임을 연결하고 결과를 반환
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/')
 def index():
     return render_template('upload.html')
 
-
-
-
-@app.route('/upload')
-def upload():
-    if 'logged_in' not in session or not session['logged_in']:
-        return redirect(url_for('index'))
-    return render_template('upload.html')
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(get_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(camera.get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
+@app.route('/capture', methods=['POST'])
+def capture():
+    image_path = camera.capture()
+    if image_path:
+        return jsonify({'success': True, 'image_rul' : image_path})
+    return jsonify({'success': False})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
---
+++ camera.py
@@ -0,0 +1,35 @@
+import cv2
+import time
+
+
+class Camera:
+    def __init__(self):
+        self.camera = cv2.VideoCapture(0)  # 카메라 연결
+
+    def get_frames(self):  # 클래스 메서드 제거
+        while True:
+            success, frame = self.camera.read()  # self.camera 사용
+            cv2.waitKey(11)  # 11ms 대기
+
+            if not success:
+                yield b'fail :('  # 바이트 타입으로 반환
+                continue
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)  # 프레임을 JPEG로 변환
+                frame = buffer.tobytes()
+
+                # 프레임을 HTTP 응답으로 변환
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    def capture(self):
+        success, frame = self.camera.read()
+        if success:
+            filename = f'static/capture_{int(time.time())}.jpg'
+            cv2.imwrite(filename, frame)
+            return filename
+        return None
+
+
+# 전역 카메라 인스턴스 생성
+camera = Camera()
